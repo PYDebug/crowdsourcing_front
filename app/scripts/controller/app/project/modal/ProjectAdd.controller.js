59ec3259cf86b13355415131231a0dbf7212490e
PYDebug <panyan5617998@163.com>
2016-09-06 21:58:58 +0800

    添加新建项目弹出框

--- app/scripts/controller/app/project/modal/ProjectAdd.controller.js
+++ app/scripts/controller/app/project/modal/ProjectAdd.controller.js
@@ -3,11 +3,43 @@
 app.controller('ProjectAddController', ['$scope', '$state', '$uibModalInstance', '$stateParams', 'ToasterTool',  'ProjectFactory',  function($scope,
     $state, $uibModalInstance, $stateParams, ToasterTool, ProjectFactory) {
 
+    $scope.user = {
+      "avatarUrl": "images/avatar1.jpg",
+      "name": "py"
+    }
+
+    $scope.users = [];
+
     init();
 
     function init(){
       console.log($state);
       console.log('ready to get yardstick code content!');
+      $scope.selectUser  = selectUser;
+      loadUsers();
+    }
+
+    function loadUsers(){
+      $scope.users.push(
+        {
+          "avatarUrl": "images/avatar1.jpg",
+          "name": "py"
+        }
+      );
+
+      $scope.users.push(
+        {
+          "avatarUrl": "images/avatar2.jpg",
+          "name": "group1"
+        }
+      );
+
+      $scope.users.push(
+        {
+          "avatarUrl": "images/avatar3.jpg",
+          "name": "group2"
+        }
+      );
     }
 
     $scope.ok = function () {
@@ -18,6 +50,8 @@ app.controller('ProjectAddController', ['$scope', '$state', '$uibModalInstance',
         $uibModalInstance.dismiss('cancel');
     };
 
-
+    function selectUser(user){
+      $scope.user = user;
+    }
 
 }]);
